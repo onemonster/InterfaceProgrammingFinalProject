1d9c5582105b89ea8a1c887153876294bf71ea1f
Ion Jo <onemonsterceo@gmail.com>
2021-12-12 19:12:39 +0900

    Initial commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by 조일현 on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
