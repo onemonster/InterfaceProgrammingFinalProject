e95c2f68985f3d46481d0f88a2d44962b717d6ff
johyeon yoon <jalynneyoon@gmail.com>
2021-12-14 15:14:16 +0900

    Apply Design to PhotoGridView

---
+++ FinalProject/Model/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  FinalProject
+//
+//  Created by Johyeon Yoon on 2021/12/14.
+//
+
+import Foundation
+import UIKit
+
+
+enum Constants {
+    enum PhotoGrid {
+        public static let photoWidth = CGFloat(175.0)
+        public static let photoHeight = CGFloat(175.0)
+        public static let profileImageCircleSize = CGFloat(35.0)
+        public static let defaultProfileImageSize = CGFloat(25.0)
+        public static let locationFontSize = CGFloat(10.0)
+        public static let userIdFontSize = CGFloat(12.0)
+    }
+}
--- FinalProject/View/ContentView.swift
+++ FinalProject/View/ContentView.swift
@@ -10,30 +10,89 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var viewModel = ViewModel()
-    
-    let columns = [
-        GridItem(spacing: 10),
-        GridItem(),
-    ]
-    
+
     var body: some View {
         VStack {
+            SearchBarView(viewModel: viewModel)
+            ScrollView {
+               GridView(viewModel: viewModel)
+                .padding([.leading, .trailing], 15.0)
+            }.onAppear(perform: { viewModel.searchPhotos() })
+        }
+        .background(Color("GridBackgroundColor"))
+    }
+}
+
+struct SearchBarView : View {
+    
+    @ObservedObject var viewModel : ViewModel
+    
+    var body: some View {
+        HStack {
             TextField("Search here", text: $viewModel.keyword, onCommit: {
                 viewModel.searchPhotos()
             })
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 5.0) {
-                    ForEach(Array(viewModel.photos.enumerated()), id: \.0) { (index, photo) in
-                        AsyncImage(url: URL(string: photo.thumbnailUrl))
-                            .onAppear(perform: {
-                                viewModel.loadMore(index: index)
-                            })
-                    }
-                }
-            }.onAppear(perform: { viewModel.searchPhotos() })
+                .font(Font.custom("NotoSansKR-Bold", size: 15.0))
+            Button(action: {
+                viewModel.searchPhotos()
+            }) {
+                Image("MagnifyingGlassImage")
+                    .foregroundColor(.black)
+            }
         }
+        .padding([.leading, .trailing], 15.0)
+        .padding(.top, 12.0)
+        .padding(.bottom, 13.0)
+        .background(.white)
+        .cornerRadius(10.0)
+        .shadow(color: Color("ShadowColor"), radius: 4.0, x: 3.0, y: 3.0)
+        .padding(15.0)
     }
 }
+
+
+struct GridView : View {
+    
+    @ObservedObject var viewModel : ViewModel
+    
+    let columns = [
+        GridItem(spacing: 10),
+        GridItem()
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 10.0) {
+            ForEach(Array(viewModel.photos.enumerated()), id: \.0) { (index, photo) in
+                AsyncImage(url: URL(string: photo.thumbnailUrl)) { image in
+                    image
+                        .resizable(resizingMode : .tile)
+                        .cornerRadius(10.0)
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: Constants.PhotoGrid.photoWidth,
+                       height: Constants.PhotoGrid.photoHeight)
+                .overlay(
+                    PhotoInfoView(viewModel: viewModel,
+                                  locationFontSize: Constants.PhotoGrid.locationFontSize,
+                                  userIdFontSize: Constants.PhotoGrid.userIdFontSize,
+                                  location: photo.user.location,
+                                  userName: photo.user.name,
+                                  profileURL: URL(string : photo.user.profileImageUrl)
+                                 )
+                        .padding([.leading, .bottom], 10.0),
+                    alignment: .bottomLeading
+                )
+                .onAppear(perform: {
+                    viewModel.loadMore(index: index)
+                })
+            }
+        }
+        
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
