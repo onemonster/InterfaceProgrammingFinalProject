bff98a9a5486ac64f9238ab863d18eb5ec87bca9
powerStone369 <64771765+powerStone369@users.noreply.github.com>
2021-12-19 13:12:38 +0900

    commit

---
+++ FinalProject/View/ImageView.swift
@@ -0,0 +1,31 @@
+//
+//  ImageView.swift
+//  FinalProject
+//
+//  Created by 오인규_일반  on 2021/12/19.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+//    @EnvironmentObject var contentView: ContentView()
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        ZStack {
+            Color.black
+            
+
+
+
+
+
+            
+        }
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
